4c235e2d35add47784b40fc00182bae5e71cfe67
FaustinaBrunoFHNW <faustina.bruno@students.fhnw.ch>
2020-03-06 19:55:42 +0100

    ResetAccountCommand Review -codestyle und javadoc

--- src/main/java/ch/brugg/fhnw/btm/command/ResetAccountsCommand.java
+++ src/main/java/ch/brugg/fhnw/btm/command/ResetAccountsCommand.java
@@ -9,7 +9,13 @@ import org.slf4j.LoggerFactory;
 
 import java.sql.Timestamp;
 import java.util.Date;
+//JAVADOC
 
+/**
+ * Command Pattern fürs
+ *
+ * @Author Faustina Bruno, Serge-Jurij Maikoff
+ */
 public class ResetAccountsCommand implements Command {
     private Logger log = LoggerFactory.getLogger(ResetAccountsCommand.class);
     private JsonAccountHandler jsonAccountHandler = JsonAccountHandler.getInstance();
@@ -17,20 +23,30 @@ public class ResetAccountsCommand implements Command {
 
     private Timestamp timestamp;
 
-    public ResetAccountsCommand(){
+    /**
+     * Constuctor
+     * Timestamp vom letzten Reset wird aus den DefaultSettings geholt, falls dieser noch nie gesetzt wurde, wird auf
+     * den jetzigen Zeitpunkt gesetzt.
+     * Ein neuer Timestamp wird anhand dem Timestamp in den DefaultSettings generiert
+     */
+    public ResetAccountsCommand() {
         super();
         timestamp = new Timestamp(System.currentTimeMillis());
         Timestamp lastReset = jsonDefaultSettingsHandler.getDefaultSettings().getTimestampLastReset();
-        if (lastReset == null){
+        if (lastReset == null) {
             log.error("Zeitstempel in DefaultSettings ist Null");
             lastReset = new Timestamp(System.currentTimeMillis());
             jsonDefaultSettingsHandler.writeDefaultSettings();
         }
+        //TODO auf Deutsch oder weg
         //Timestamp is dependent on timestamp in DefaultSettings. If not present, a new one is created dependent on now
-        this.timestamp.setTime(lastReset.getTime() + 60* 1000 * jsonDefaultSettingsHandler.getDefaultSettings().getResetInterval());
+        this.timestamp.setTime(
+                lastReset.getTime() + 60 * 1000 * jsonDefaultSettingsHandler.getDefaultSettings().getResetInterval());
     }
 
-
+    /**
+     * In dieser Methode werden bei allen zertifizierten Accounts die Counters zurückgesetzt
+     */
     private void setAllCountersToMax() {
 
         for (JsonAccount jsonAccount : this.jsonAccountHandler.getJsonAccountList()) {
@@ -40,15 +56,21 @@ public class ResetAccountsCommand implements Command {
 
     }
 
-    @Override
-    public Timestamp getTimestamp() {
+    @Override public Timestamp getTimestamp() {
         return timestamp;
     }
 
-    @Override
-    public void execute() {
+    //TODO JAVADOC
+
+    /**
+     * In dieser Methode werden die Methode um alle Counters zurückzusetzten und den Timestamp zu setzen gestartet,
+     * wie auch beide  Methoden um die Accounts wie auch die DefaultSettings in die Dateien zu speichern.
+     * Am Ende wird ein neuer Command iniziert um bei der im Timestamp gespeicherten Zeit wieder den gleichen
+     * Command auszuführen
+     */
+    @Override public void execute() {
         log.info(new Date().toString());
-        setAllCountersToMax();
+        this.setAllCountersToMax();
         this.jsonDefaultSettingsHandler.getDefaultSettings()
                 .setTimestampLastReset(new Timestamp(System.currentTimeMillis()));
         this.jsonAccountHandler.writeAccountList();
