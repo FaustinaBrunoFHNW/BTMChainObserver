a1060e26d8d3ef7a20f36a6ed57bb915a51d2730
FaustinaBrunoFHNW <faustina.bruno@students.fhnw.ch>
2020-03-08 12:42:22 +0100

    Unnötige andere Klassen Instanzen gelöscht

--- src/main/java/ch/brugg/fhnw/btm/SubscriptionTX.java
+++ src/main/java/ch/brugg/fhnw/btm/SubscriptionTX.java
@@ -27,18 +27,15 @@ public class SubscriptionTX {
 
     private Web3j web3j;
     private  Logger log = LoggerFactory.getLogger(SubscriptionTX.class);
-    private ChainInteractions chainInteractions;
     private JsonAccountHandler accountHandler = JsonAccountHandler.getInstance();
-    private JsonDefaultSettingsHandler jsonDefaultSettingsHandler = JsonDefaultSettingsHandler.getInstance();
     private DoSAlgorithm dosAlgorithm = DoSAlgorithm.getInstance();
 
     /**
      * Constructor der Klasse
-     * @param chainInteractions instanzertes Objekt
+     * @param chainInteractions instanzertes Objekt fpr die Weitergabe
      */
     public SubscriptionTX( ChainInteractions chainInteractions) {
         this.web3j = ChainSetup.getInstance().getWeb3j();
-        this.chainInteractions = chainInteractions;
         this.dosAlgorithm.setChainInteractions(chainInteractions);
     }
 
@@ -60,29 +57,22 @@ public class SubscriptionTX {
      */
     private void txFilter() {
         Disposable subscription = this.web3j.transactionFlowable().subscribe(tx -> {
-            //TODO Gas pro Zeiteinheit anzeigen
             this.log.info("Eine Transaktion von folgender Adresse wurde gefunden: " + tx.getFrom());
-            this.log.info("Gas price used: " + tx.getGasPrice());
+            this.log.info("Gas price: " + tx.getGasPrice());
             this.log.info("Gas : " + tx.getGas());
-            this.log.info("Gas : " + tx.getGasRaw());
+            this.log.info("Gas Raw: " + tx.getGasRaw());
             this.log.info("Transferierter Ether: " + Convert.fromWei(tx.getValue().toString(), Convert.Unit.ETHER));
 
-            //TODO Admin Account darf unendlich viele Transaktionen machen (ID von Account == dann nicht revoked)
             if (tx.getGasPrice().equals(BigInteger.ZERO)) {
                 this.log.info("Transaktionskosten waren 0");
-
                 this.dosAlgorithm.dosAlgorithm(this.accountHandler.processAccount(tx.getFrom(), tx.getGas()));
-
             }
         });
     }
 
     /**
-     * In dieser Methode werden alle Counters zurückgesetzt
+     * In dieser Methode wird der Command um alle Counters zurückzusetzten in die Queue hinzugefügt
      */
-
-
-
     private void intervall()  {
         this.log.info("*************Intervall ist gestartet********************");
         DoSAlgorithm.getInstance().offerCommand(new ResetAccountsCommand());
