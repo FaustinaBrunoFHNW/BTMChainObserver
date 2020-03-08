38d75234661aa4714cd560216206b9dc6a7fa9bf
FaustinaBrunoFHNW <faustina.bruno@students.fhnw.ch>
2020-03-08 12:02:39 +0100

    Javadoc ergänzt logs todos abgearbietet KONSTANTEN

--- src/main/java/ch/brugg/fhnw/btm/dosAlgorithm/DoSAlgorithm.java
+++ src/main/java/ch/brugg/fhnw/btm/dosAlgorithm/DoSAlgorithm.java
@@ -15,11 +15,12 @@ import java.util.Queue;
 
 import static java.util.Comparator.comparing;
 
-//TODO JavaDov ergänzen
 /**
  * In dieser Klasse ist der Dos Algorithmus implementiert, der gratis Transaktionen überwacht
  * Der Account der die Transaktion auslöst hat eine max. Anzahl Transaktionen und Gas die er benutzen kann,
- * bis er gesperrt wird
+ * bis er gesperrt wird, dies wird hier kontrolliert.
+ * Falls ein Account gesperrt wird, wird er mit einem Timestamp und dem CertifyCommand in die Queue gespeichert
+ * um zur gespeicherten Zeit wieder zertifiziert zu werden
  *
  * @Author Faustina Bruno, Serge-Jurij Maikoff
  */
@@ -30,6 +31,11 @@ public class DoSAlgorithm {
     private JsonDefaultSettingsHandler defaultSettingsHandler =JsonDefaultSettingsHandler.getInstance();
     private  Logger log = LoggerFactory.getLogger(DoSAlgorithm.class);
     private Queue<Command> queue = new PriorityQueue<>(comparing(Command::getTimestamp));
+
+    //*************KONSTANEN****************
+    private static long SLEEP_TIME=1000;
+    private static int MILLISEC_IN_SEC=1000;
+    private static int SEC_IN_MIN=60;
 
     /**
      * Getter um die Instanz des DoSAlgorithmus zu holen
@@ -80,29 +86,32 @@ public class DoSAlgorithm {
 
         Timestamp tempStamp = new Timestamp(System.currentTimeMillis());
         long temp = tempStamp.getTime();
+        //Abfrage ob der Account noch genügend Transaktionen hat
         if (jsonAccount.getRemainingTransactions() == 0) {
             this.chainInteractions.revokeAccount(jsonAccount.getAddress());
-           long revokeTime = jsonAccount.getRevokeTime().intValue() *60 * 1000;
+           long revokeTime = jsonAccount.getRevokeTime().intValue() *SEC_IN_MIN * MILLISEC_IN_SEC;
 
             tempStamp.setTime(temp + revokeTime);
             jsonAccount.setTimeStamp(tempStamp);
 
+            //Account wird in Priorityqueue hinzugefügt
             this.queue.add(new CertifyCommand(jsonAccount));
-            //TODO Account in Priority Queue werfen
+
             this.log.info("Der Acccount "+ jsonAccount.getAddress()+" hat zu viele Transaktionen getätigt und wurde gesperrt. " +
                     " Die Sperrung  wird um "+ tempStamp.toString() +" aufgehoben ");
 
             this.accountHandler.writeAccountList();
 
+            //Abfrage ob der Account noch genügend Gas hat
         }  if (jsonAccount.getRemainingGas() < 0) {
             this.chainInteractions.revokeAccount(jsonAccount.getAddress());
+            //TODO Konstante bennen
             long revokeTime = jsonAccount.getRevokeTime().intValue()*60 * 1000;
             tempStamp.setTime(temp + revokeTime);
 
             jsonAccount.setTimeStamp(tempStamp);
             this.queue.add(new CertifyCommand(jsonAccount));
 
-            //TODO Account in Priority Queue werfen
             this.log.info("Der Acccount "+ jsonAccount.getAddress()+" hat zu viel Gas verbraucht und wurde gesperrt. " +
                     " Die Sperrung  wird um "+ tempStamp.toString() +" aufgehoben ");
 
@@ -111,10 +120,20 @@ public class DoSAlgorithm {
         this.log.info("Account Datei wird aktualisiert");
         JsonAccountHandler.getInstance().writeAccountList();
     }
-    //TODO JAVADOC
-    public void offerAccount(JsonAccount acc){
-        this.queue.add(new CertifyCommand(acc));
+
+
+    /**
+     * Ein Account wird mit dem Certify Command in die Queue hinzugefügt
+     * @param account welcher zertifiziert/ in die Whiteliste aufgenommen werden soll
+     */
+    public void offerAccount(JsonAccount account){
+        this.queue.add(new CertifyCommand(account));
     }
+
+    /**
+     * Hier wird die Queue gestartet. Es gelicht den gespeicherten Timestamp mit der jetztigen Zeit ab. Falls
+     * die gespeicherte Zeit gleich ist oder in der Vergangenheit, wird der gespeicherte Command ausgeführt.
+     */
     private Runnable queueInspector = () -> {
         this.log.info("Queue wurde gestartet und läuft.");
         while (true) {
@@ -127,17 +146,19 @@ public class DoSAlgorithm {
 
             }
             try {
-                //TODO wieso 1000, müssen Zahlen als Konstanten definieren
-                Thread.sleep(1000);
+                Thread.sleep(SLEEP_TIME);
             } catch (InterruptedException e) {
-                //TODO error Meldung schreiben
-                this.log.error("");
+                this.log.error("Ein Fehler ist beim Ausführen des Commands in der Queue geschehen");
                 e.printStackTrace();
             }
 
         }
     };
 
+    /**
+     *  Dies Methode fügt der Queue ein Command hinzu
+      * @param command der in die Queue hinzugefügt wird um ausgeführt zu werden
+     */
     public void offerCommand(Command command){
         queue.add(command);
     }
