e150c9cb9e9b7efba573192128db29b0c34fc627
FaustinaBrunoFHNW <faustina.bruno@students.fhnw.ch>
2020-03-11 11:58:37 +0100

    Test Methoden vorbereitet

---
+++ src/test/java/ch/brugg/fhnw/btm/CertifierNurEinmalDeployenTest.java
@@ -0,0 +1,13 @@
+package ch.brugg.fhnw.btm;
+
+import org.junit.Test;
+
+public class CertifierNurEinmalDeployenTest {
+
+    @Test
+    public static void certifierDeployOnce(){}
+
+
+    @Test
+    public static void certifierDeployTwice(){}
+}
