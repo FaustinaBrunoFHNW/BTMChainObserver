1645e4791d7e9eda60b85e65acff512376ebacc6
FaustinaBrunoFHNW <faustina.bruno@students.fhnw.ch>
2020-03-11 20:04:57 +0100

    Neuer Test gestartet

---
+++ src/test/java/ch/brugg/fhnw/btm/TransactionManagerAccountNotRevokeTest.java
@@ -0,0 +1,17 @@
+package ch.brugg.fhnw.btm;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TransactionManagerAccountNotRevokeTest {
+
+    @Test
+    public void tmOneTxMoreThenLimit(){
+        Assert.assertFalse(true);
+        }
+
+        @Test
+    public void tmDoubleTxThenLimit(){
+        Assert.assertFalse(true);
+        }
+}
